a20a9ee4073d6f3442372a8c80e88be9ad0441c2
mkniesel@anl.gov <mkniesel@anl.gov>
2022-11-17 09:09:18 -0600

    changes

---
+++ collab.c
@@ -0,0 +1,36 @@
+// Collaborative code file, Lab 13
+//
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this
+// code!
+//
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.dddddd
+//
+// An example is shown below.
+//
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void mknie2_dcard4() {
+  printf("Mathias Kniesel and David Cardenas\n");
+  printf("We learned that git is a useful tool for colabertion and when "
+         "merging two branches together, a merge conflict may occur if the two "
+         "branches contain differing content on a given line/s. This is fixed "
+         "manually by editing each confliction, after this is finished the "
+         "branches may be successfully merged.\n");
+}
+
+int main() {
+  printf(
+      "This code has been created by the students of CS 211, Fall 2022!\n\n");
+
+  mknie2_dcard4();
+
+  return 0;
+}
